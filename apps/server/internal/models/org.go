f12931becceb48589373b6314ad3a4494b446451
josephliao542211 <billan452211@gmail.com>
2026-03-25 21:33:00 -0400

    fixed naming

--- apps/server/internal/models/org.go
+++ apps/server/internal/models/org.go
@@ -9,8 +9,8 @@ import (
 type Org struct {
 	ID         uuid.UUID `json:"id" gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
 	OrgName    string    `json:"org_name" gorm:"not null;uniqueIndex"`
-	D2LOrgID   string    `json:"d2l_org_id" gorm:"not null;uniqueIndex"`
-	D2LBaseURL string    `json:"d2l_base_url" gorm:"not null;uniqueIndex"`
+	D2LOrgID   string    `json:"d2l_org_id" gorm:"column:d2l_org_id;not null;uniqueIndex"`
+	D2LBaseURL string    `json:"d2l_base_url" gorm:"column:d2l_base_url;not null;uniqueIndex"`
 	LEVersion  *string   `json:"le_version"`
 	LPVersion  *string   `json:"lp_version"`
 	CreatedAt  time.Time `json:"created_at" gorm:"type:timestamptz;default:now()"`
